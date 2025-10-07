ba5ab2e951217a41ede3a7dc94dc2a86fa52d73a
Kristof <foldikristof20@gmail.com>
2025-10-07 21:30:43 +0200

    Frissítés: új fájlok és módosítások

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ $(function () {
   // megjelenítés az articlebe
   megjelenites(txt, $("article"));
 });
+
+function toggleKeresosav() {
+  const sav = document.getElementById("mobilKeresosav");
+  if (sav.style.maxHeight) {
+    sav.style.maxHeight = null;
+    sav.style.opacity = 0;
+  } else {
+    sav.style.maxHeight = sav.scrollHeight + "px";
+    sav.style.opacity = 1;
+  }
+}
+
+function keresesInditasa() {
+  const mezo = document.getElementById("keresoMezo");
+  const keresettSzoveg = mezo.value.trim();
+  if (keresettSzoveg === "") {
+    alert("Kérlek, írj be valamit a kereséshez!");
+  } else {
+    alert("Keresett kifejezés: " + keresettSzoveg);
+  }
+}
